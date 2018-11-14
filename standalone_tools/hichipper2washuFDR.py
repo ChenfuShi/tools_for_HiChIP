29d232cc36477ff9f4ab91a650420e430d57fa7a
ChenfuShi <40122452+ChenfuShi@users.noreply.github.com>
2018-11-14 19:35:05 +0000

    a

--- standalone_tools/hichipper2washuFDR.py
+++ standalone_tools/hichipper2washuFDR.py
@@ -28,5 +28,5 @@ with gzip.open(outputname, "wt", compresslevel=5) as outputfile, open(args.input
     for line in inputfile:
         data = line.split("\t")
 
-        outputfile.write("{},{},{}\t{},{},{}\t{}".format(data[0],data[1],data[2],data[3],data[4],data[5],str(1-int(data[7].strip()))))
+        outputfile.write("{},{},{}\t{},{},{}\t{}".format(data[0],data[1],data[2],data[3],data[4],data[5],str(1-float(data[7].strip()))))
 
